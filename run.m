07ae34fb4b5842e8566a77dd5dc2751119d00c23
Matthew Cullen Self <cullen.cs@gmail.com>
2016-12-07 21:12:20 -0600

    Restructured Project into labeled subdirectories to organize code

--- run.m
+++ run.m
@@ -1,3 +1,9 @@
+clear; close all; clc;
+
+addpath('./code');
+addpath('./code/helpers');
+addpath('./config');
+
 rx.RXNoise = -4.5; % dB-Hz
 rx.r = [3578600000,0,0]'; % m
 rx.u = -rx.r/norm(rx.r); % unit vector representing rx antenna orientation
@@ -6,4 +12,4 @@ rx.RXGain = 5; % dB
 gpsWeek = 1920;
 gpsSec = 345680;
 
-[ tracked, neededGain ] = observe( rx , gpsWeek , gpsSec )
+[ tracked, neededGain ] = observe( rx , gpsWeek , gpsSec );
