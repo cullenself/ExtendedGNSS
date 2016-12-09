66d7ec743ace7298cb31020d115837a8f3115ac5
Matthew Cullen Self <cullen.cs@gmail.com>
2016-12-09 03:36:37 -0600

    Generated Pictures, modified testing code

--- tests/repeatAO40.m
+++ tests/repeatAO40.m
@@ -10,9 +10,38 @@ rx.u = -rx.r/norm(rx.r); % unit vector representing rx antenna orientation
 rx.Tsys = 190; % Kelvin ***double check this number
 rx.RXGain = 4; % dB
 gpsWeek = 1920;
-gpsSec = 345680+5000;
-thresh = 42;
+gpsSec = 345680;
+thresh = 40;
 
 satdata = retrieveNavigationData(gpsWeek,gpsSec,0,'../NavFiles');
+gpsSecVec = gpsSec+(1:48*4)*15*60;
+for i = 1:(48*4)
+    [ tracked, neededGain(i) ] = observe( rx , gpsWeek , gpsSecVec(i), retrieveNavigationData(gpsWeek,gpsSecVec(i),0,'../NavFiles') , thresh );
+    numSats(i) = numel(tracked);
+end
 
-[ tracked, neededGain ] = observe( rx , gpsWeek , gpsSec , satdata , thresh ); 
+figure(1);
+stairs(gpsSecVec,numSats);
+xlabel('GPS Second');
+ylabel('Number of Satellites');
+title('Satellites Tracked vs Time for AO40 Replication');
+print('../Images/ao40actual','-dpng');
+mean(numSats)
+
+% Lower the threshold
+thresh = 25;
+
+satdata = retrieveNavigationData(gpsWeek,gpsSec,0,'../NavFiles');
+gpsSecVec = gpsSec+(1:48*4)*15*60;
+for i = 1:(48*4)
+    [ tracked, neededGain(i) ] = observe( rx , gpsWeek , gpsSecVec(i), retrieveNavigationData(gpsWeek,gpsSecVec(i),0,'../NavFiles') , thresh );
+    numSats(i) = numel(tracked);
+end
+
+figure(2)
+stairs(gpsSecVec,numSats);
+xlabel('GPS Second');
+ylabel('Number of Satellites');
+title('Satellites Tracked vs Time for Modified AO40 Replication');
+print('../Images/ao40better','-dpng');
+mean(numSats)
--- tests/singlesat.m
+++ tests/singlesat.m
@@ -12,14 +12,13 @@ config;
 satdata = retrieveNavigationData(gpsWeek,gpsSec,0,'../NavFiles');
 
 sat53 = satdata([satdata.SVID]==31);
-sat53.i0 = 0;
 
 [sv.r, sv.v] = satloc(gpsWeek,gpsSec,sat53);
 sv.u = -sv.r/norm(sv.r);
 
 for i = 1:1000
     d(i) = ((i-1)*(385000-20000)/1000+20000)*1000;
-    sv.rel = sv.r - [10000;0;d(i)];
+    sv.rel = sv.r - [0;0;d(i)];
     [phi(i), theta(i)] = findRotationAngle(sv, gpsWeek, gpsSec);
     if (abs(theta(i)) < EarthCutoff) || (abs(theta(i)) > 90)
         visible(i).power = NaN;
@@ -30,42 +29,75 @@ for i = 1:1000
     end
 end
 figure(1);
-plot(d,phi);
+plot(d./1000-6378,phi);
+xlabel('Height above North Pole (km)');
+ylabel('Phi (deg)');
+title('Phi vs Height for SVID 31 at GPS Time: 1920:345680');
+print('../images/phivsheight','-dpng');
 figure(2);
-plot(d,theta);
+plot(d./1000-6378,theta);
+xlabel('Height above North Pole (km)');
+ylabel('Theta (deg)');
+title('Theta vs Height for SVID 31 at GPS Time: 1920:345680');
+print('../images/thetavsheight','-dpng');
 figure(3);
-plot(d,[visible.cn0]);
-
+plot(d./1000-6378,[visible.cn0]);
+xlabel('Height above North Pole (km)');
+ylabel('Unbiased C/N0 (db-Hz)');
+title('C/N0 vs Height for SVID 31 at GPS Time: 1920:345680');
+print('../images/cn0vsheight','-dpng');
 
 clearvars('phi','theta');
 [X,Y] = meshgrid(-100000:1000:100000);
 X = X*1000;
 Y = Y*1000;
-sv.r = [20000000;0;0];
-sv.u = [-1;0;0];
-sv.v = [0;sqrt(3.986004418e14/norm(sv.r));0];
 for i = 1:size(X,1)
     for j = 1:size(Y,1)
         sv.rel = sv.r - [X(1,i);Y(j,1);0];
         [phi(j,i), theta(j,i)] = findRotationAngle(sv, gpsWeek, gpsSec);
-        if (abs(theta(j,i)) < EarthCutoff) || (abs(theta(j,i)) > 90)
+        if (abs(theta(j,i)) < EarthCutoff) || (abs(theta(j,i)) >= 90)
             cn0(j,i) = NaN;
             continue;
         end
-        power = SatPowerOut(31) + SatDirectivityGain(dirGain,theta(j,i), phi(j,i)) + SatGainCF(31) + 20*log10(lambdaGPSL1/(4*pi*norm(sv.rel))); % multiple things need to be checked out, log vs log10, need to make SatPowerOut matrix, SatDGain depends on another angle that I don't know, must make up RXGain, need to cite this formula
-        cn0(j,i) = power - 10*log10(190) + 228.6; % RXNoise is negative
-        if cn0(j,i) > 100
-            disp('help');
-        end
+        power = SatPowerOut(31) + SatDirectivityGain(dirGain,theta(j,i), phi(j,i)) + SatGainCF(31) + 20*log10(lambdaGPSL1/(4*pi*norm(sv.rel)));
+        cn0(j,i) = power - 10*log10(190) + 228.6;
     end
 end
 figure(4)
-surf(X,Y,cn0);
+surfc(X./1000,Y./1000,cn0);
 hold on;
-scatter3(sv.r(1),sv.r(2),sv.r(3),500);
-scatter3(0,0,0,6378);
+scatter3(sv.r(1)/1000,sv.r(2)/1000,0,1);
+scatter3(0,0,0,10);
+xlabel('ECEF x (km)');
+ylabel('ECEF y (km)');
+zlabel('Unbiased C/N0 Ratio (db-Hz)');
+title('Unbiased C/N0 Map for SVID 31 at GPS Time: 1920:345680');
+
+sv.r = [20000000;0;0];
+sv.u = [-1;0;0];
+sv.v = [0;sqrt(3.986004418e14/norm(sv.r));1];
+clearvars('phi','theta');
+[X,Y] = meshgrid(-100000:1000:100000);
+X = X*1000;
+Y = Y*1000;
+for i = 1:size(X,1)
+    for j = 1:size(Y,1)
+        sv.rel = sv.r - [X(1,i);Y(j,1);0];
+        [phi(j,i), theta(j,i)] = findRotationAngle(sv, gpsWeek, gpsSec);
+        if (abs(theta(j,i)) < EarthCutoff) || (abs(theta(j,i)) >= 90)
+            cn0(j,i) = NaN;
+            continue;
+        end
+        power = SatPowerOut(31) + SatDirectivityGain(dirGain,theta(j,i), phi(j,i)) + SatGainCF(31) + 20*log10(lambdaGPSL1/(4*pi*norm(sv.rel)));
+        cn0(j,i) = power - 10*log10(190) + 228.6;
+    end
+end
 figure(5)
-surf(X,Y,theta);
+surfc(X./1000,Y./1000,cn0);
 hold on;
-scatter3(sv.r(1),sv.r(2),sv.r(3),500);
-scatter3(0,0,0,6378);
+scatter3(sv.r(1)/1000,sv.r(2)/1000,1);
+scatter3(0,0,0,10);
+xlabel('ECEF x (km)');
+ylabel('ECEF y (km)');
+zlabel('Unbiased C/N0 Ratio (db-Hz)');
+title('Unbiased C/N0 Map for Ideal Satellite on ECEF x-axis');
