82254b8a03643f601181b222ee1569ca411c381f
Matthew Cullen Self <cullen.cs@gmail.com>
2016-12-09 00:59:24 -0600

    Fixed angles, gain, and made singlesat.m test

--- code/findRotationAngle.m
+++ code/findRotationAngle.m
@@ -13,21 +13,27 @@ u_midnight_ecef = -e_sun_ecef - (dot(-e_sun_ecef,e_h)*e_h);
 mu = acos(dot(u_midnight_ecef,sat.r)/(norm(u_midnight_ecef) * norm(sat.r))); % radians
 % find yaw angle, igs
 yaw_igs = atan2(-tan(beta),sin(mu)); % radians
-Q_rtn_bf = rotate(yaw_igs,3);
-Q_rtn_ecef = [ (sat.r/norm(sat.r))' ; (cross(e_h,sat.r/norm(sat.r)))' ; e_h' ];
+Q_rtn_bf = rotate(-yaw_igs,3);
+Q_rtn_ecef = [ sat.u' ; (cross(e_h,sat.r/norm(sat.r)))' ; e_h' ]*[0 0 1;1 0 0;0 1 0]';
 % turn yaw angle to pointing angle
-x_sv_ecef = Q_rtn_ecef' * Q_rtn_bf' * [-1; 0; 0];
-y_sv_ecef = Q_rtn_ecef' * Q_rtn_bf' * [0; -1; 0];
-z_sv_ecef = Q_rtn_ecef' * Q_rtn_bf' * [0; 0; 1];
+x_sv_ecef = Q_rtn_ecef' * Q_rtn_bf' * [1; 0; 0];
+y_sv_ecef = Q_rtn_ecef' * Q_rtn_bf' * [0; 1; 0];
+z_sv_ecef = Q_rtn_ecef' * Q_rtn_bf' * [0; 0; -1];
 % change pointing angle to phi
 rel_xy = sat.rel - dot(sat.rel,z_sv_ecef)*z_sv_ecef/(norm(z_sv_ecef)^2);
-phiDeg = acosd(dot(x_sv_ecef,rel_xy)/(norm(x_sv_ecef) * norm(rel_xy))); % **Disambiguate**
-if dot(y_sv_ecef,rel_xy) < 0 % based on how Lockheed defines angles
+phiDeg = acosd(dot(x_sv_ecef,rel_xy)/(norm(x_sv_ecef) * norm(rel_xy)));
+if dot(y_sv_ecef,rel_xy) > 0 % based on how Lockheed defines angles
     phiDeg = 360-phiDeg;
 end
+if norm(rel_xy) == 0
+    phiDeg = 0;
+end
 rel_zy = sat.rel - dot(sat.rel,x_sv_ecef)*x_sv_ecef/(norm(x_sv_ecef)^2);
-offboreDeg = acosd(dot(z_sv_ecef,rel_zy)/(norm(z_sv_ecef) * norm(rel_zy)));
+offboreDeg = acosd(dot(-z_sv_ecef,rel_zy)/(norm(z_sv_ecef) * norm(rel_zy)));
 if dot(y_sv_ecef,rel_zy) > 0 % based on how Lockheed defines angles
     offboreDeg = -offboreDeg;
+end
+if norm(rel_zy) == 0
+    offboreDeg = 180;
 end
 end
--- tests/singlesat.m
+++ tests/singlesat.m
@@ -12,22 +12,60 @@ config;
 satdata = retrieveNavigationData(gpsWeek,gpsSec,0,'../NavFiles');
 
 sat53 = satdata([satdata.SVID]==31);
+sat53.i0 = 0;
 
 [sv.r, sv.v] = satloc(gpsWeek,gpsSec,sat53);
 sv.u = -sv.r/norm(sv.r);
 
 for i = 1:1000
     d(i) = ((i-1)*(385000-20000)/1000+20000)*1000;
-    sv.rel = sv.r - [d(i);0;0];
+    sv.rel = sv.r - [10000;0;d(i)];
     [phi(i), theta(i)] = findRotationAngle(sv, gpsWeek, gpsSec);
-    visible(i).power = SatPowerOut(31) + SatDirectivityGain(dirGain,theta(i), phi(i)) + SatGainCF(31) + 20*log10(lambdaGPSL1/(4*pi*norm(sv.rel))); % multiple things need to be checked out, log vs log10, need to make SatPowerOut matrix, SatDGain depends on another angle that I don't know, must make up RXGain, need to cite this formula
-    visible(i).cn0 = visible(i).power - 10*log10(190) + 228.6; % RXNoise is negative
+    if (abs(theta(i)) < EarthCutoff) || (abs(theta(i)) > 90)
+        visible(i).power = NaN;
+        visible(i).cn0 = 0;
+    else
+        visible(i).power = SatPowerOut(31) + SatDirectivityGain(dirGain,theta(i), phi(i)) + SatGainCF(31) + 20*log10(lambdaGPSL1/(4*pi*norm(sv.rel))); % multiple things need to be checked out, log vs log10, need to make SatPowerOut matrix, SatDGain depends on another angle that I don't know, must make up RXGain, need to cite this formula
+        visible(i).cn0 = visible(i).power - 10*log10(190) + 228.6; % RXNoise is negative
+    end
 end
-
+figure(1);
 plot(d,phi);
-
-figure
+figure(2);
 plot(d,theta);
-
-figure
+figure(3);
 plot(d,[visible.cn0]);
+
+
+clearvars('phi','theta');
+[X,Y] = meshgrid(-100000:1000:100000);
+X = X*1000;
+Y = Y*1000;
+sv.r = [20000000;0;0];
+sv.u = [-1;0;0];
+sv.v = [0;sqrt(3.986004418e14/norm(sv.r));0];
+for i = 1:size(X,1)
+    for j = 1:size(Y,1)
+        sv.rel = sv.r - [X(1,i);Y(j,1);0];
+        [phi(j,i), theta(j,i)] = findRotationAngle(sv, gpsWeek, gpsSec);
+        if (abs(theta(j,i)) < EarthCutoff) || (abs(theta(j,i)) > 90)
+            cn0(j,i) = NaN;
+            continue;
+        end
+        power = SatPowerOut(31) + SatDirectivityGain(dirGain,theta(j,i), phi(j,i)) + SatGainCF(31) + 20*log10(lambdaGPSL1/(4*pi*norm(sv.rel))); % multiple things need to be checked out, log vs log10, need to make SatPowerOut matrix, SatDGain depends on another angle that I don't know, must make up RXGain, need to cite this formula
+        cn0(j,i) = power - 10*log10(190) + 228.6; % RXNoise is negative
+        if cn0(j,i) > 100
+            disp('help');
+        end
+    end
+end
+figure(4)
+surf(X,Y,cn0);
+hold on;
+scatter3(sv.r(1),sv.r(2),sv.r(3),500);
+scatter3(0,0,0,6378);
+figure(5)
+surf(X,Y,theta);
+hold on;
+scatter3(sv.r(1),sv.r(2),sv.r(3),500);
+scatter3(0,0,0,6378);
